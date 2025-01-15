0c2322667a4306656c85d74908d99a474b96d476
Kian <kianluis1382@gmail.com>
2025-01-15 19:03:06 +0100

    explanations added

---
+++ boolian_operators.py
@@ -0,0 +1,11 @@
+print(not True)
+print(not False)
+print(not 7) # False because 7 is True
+
+print(True or False) # In an "or" it prints the first thing that is True
+print(True and False) # In an "and" it prints the first thing that is False
+print(2 or 3 or 4) # In an "or" it prints the first thing that is True
+print(False or 0 or 4 or True) # In an "or" it prints the first thing that it True
+print(1 and 2 and 3) # prints everything that is True until the last number
+print(1 and 0.0 and 4) # prints the value that is False
+
